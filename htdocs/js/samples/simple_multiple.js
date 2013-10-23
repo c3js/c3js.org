3d0ca49066c55409ae294e731a29153d9b35f044
Masayuki Tanaka <masayuki0812@mac.com>
2013-10-23 20:06:14 +0700

    Add tips

---
+++ htdocs/js/samples/simple_multiple.js
@@ -0,0 +1,9 @@
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 50, 20, 10, 40, 15, 25]
+        ]
+    }
+});
