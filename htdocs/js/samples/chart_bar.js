30e421a2142488b92b1ba1c0af91ca2639d5e135
Masayuki Tanaka <masayuki0812@mac.com>
2013-11-14 17:54:37 +0900

    Add sample for bar chart

---
+++ htdocs/js/samples/chart_bar.js
@@ -0,0 +1,17 @@
+var chart = c3.generate({
+    data: {
+        columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        types: {
+            data1: 'bar',
+            data2: 'bar'
+        }
+    },
+    axis: {
+        x: {
+            type: 'categorized'
+        }
+    }
+});
