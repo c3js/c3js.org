c4dd6ded6a8b07a4d6b6e2650712438219297103
Masayuki Tanaka <masayuki0812@mac.com>
2013-11-16 21:49:15 +0900

    Add sample for spline chart

---
+++ htdocs/js/samples/chart_spline.js
@@ -0,0 +1,12 @@
+var chart = c3.generate({
+    data: {
+        columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        types: {
+            data1: 'spline',
+            data2: 'spline'
+        }
+    }
+});
