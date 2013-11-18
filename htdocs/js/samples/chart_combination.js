5f5a875a092bd36d3cbf5f5dc64d083676a05534
Masayuki Tanaka <masayuki0812@mac.com>
2013-11-18 17:45:26 +0900

    Add sample for combination chart

---
+++ htdocs/js/samples/chart_combination.js
@@ -0,0 +1,26 @@
+var chart = c3.generate({
+    data: {
+        columns: [
+            ['data1', 30, 20, 50, 40, 60, 50],
+            ['data2', 200, 130, 90, 240, 130, 220],
+            ['data3', 300, 200, 160, 400, 250, 250],
+            ['data4', 200, 130, 90, 240, 130, 220],
+            ['data5', 130, 120, 150, 140, 160, 150]
+        ],
+        types: {
+            data1: 'bar',
+            data2: 'bar',
+            data3: 'spline',
+            data4: 'line',
+            data5: 'bar',
+        },
+        groups: [
+            ['data1','data2']
+        ]
+    },
+    axis: {
+        x: {
+            type: 'categorized'
+        }
+    }
+});
