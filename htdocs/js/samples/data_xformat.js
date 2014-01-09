cfffb106faffd8396b5fabca1568bd3d49643832
Masayuki Tanaka <masayuki0812@mac.com>
2014-01-09 09:40:29 +0900

    Update c3

--- htdocs/js/samples/data_xformat.js
+++ htdocs/js/samples/data_xformat.js
@@ -1,6 +1,6 @@
 var chart = c3.generate({
     data: {
-        x: 'date',           // default 'x'
+        x: 'date',
         x_format : '%Y%m%d', // default '%Y-%m-%d'
         columns: [
             ['date', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
