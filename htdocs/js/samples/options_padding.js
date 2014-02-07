e6f172b4bba78de007927af658d92433ee53af24
Masayuki Tanaka <masayuki0812@mac.com>
2014-02-08 01:03:39 +0900

    Add sample for padding

---
+++ htdocs/js/samples/options_padding.js
@@ -0,0 +1,10 @@
+var chart = c3.generate({
+    padding: {
+        left: 100,
+    },
+    data: {
+        columns: [
+            ['sample', 30, 200, 100, 400, 150, 250000000000]
+        ]
+    }
+});
