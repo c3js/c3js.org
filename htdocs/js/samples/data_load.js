2a13a53e9d2fcdc7d39a4855ce9c6b05fad1b0b3
Masayuki Tanaka <masayuki0812@mac.com>
2014-02-10 12:41:23 +0900

    Revert "Update sample for data load"
    This reverts commit bf741df2e544564e01759ff4460a83df730e78e0.

--- htdocs/js/samples/data_load.js
+++ htdocs/js/samples/data_load.js
@@ -8,7 +8,7 @@ setTimeout(function () {
     chart.load({
         url: '/data/c3_test2.csv'
     });
-}, 1000);
+}, 2000);
 
 setTimeout(function () {
     chart.load({
@@ -18,7 +18,7 @@ setTimeout(function () {
             ['data3', 300, 200, 160, 400, 250, 250]
         ]
     });
-}, 2000);
+}, 4000);
 
 setTimeout(function () {
     chart.load({
@@ -32,12 +32,4 @@ setTimeout(function () {
             [90, 220, 320],
         ]
     });
-}, 3000);
-
-setTimeout(function () {
-    chart.load({
-        columns:[
-            ['data1', 30, 20, 50, 40, 60, 50,100,200]
-        ]
-    });
-}, 4000);
+}, 6000);
