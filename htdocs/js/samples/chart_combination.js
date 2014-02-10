0fbcd453b23114cc6e020bdcd604097c72bf1e4c
Masayuki Tanaka <masayuki0812@mac.com>
2014-02-10 12:32:00 +0900

    Update c3.js and add sample for area chart

--- htdocs/js/samples/chart_combination.js
+++ htdocs/js/samples/chart_combination.js
@@ -5,14 +5,14 @@ var chart = c3.generate({
             ['data2', 200, 130, 90, 240, 130, 220],
             ['data3', 300, 200, 160, 400, 250, 250],
             ['data4', 200, 130, 90, 240, 130, 220],
-            ['data5', 130, 120, 150, 140, 160, 150]
+            ['data5', 130, 120, 150, 140, 160, 150],
+            ['data6', 90, 70, 20, 50, 60, 120],
         ],
+        type: 'bar',
         types: {
-            data1: 'bar',
-            data2: 'bar',
             data3: 'spline',
             data4: 'line',
-            data5: 'bar',
+            data6: 'area',
         },
         groups: [
             ['data1','data2']
