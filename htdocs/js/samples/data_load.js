f125c50e19f5042128abcac922550ebb9f4bc823
Masayuki Tanaka <masayuki0812@mac.com>
2014-03-05 23:03:04 +0900

    Update sample and c3.js

--- htdocs/js/samples/data_load.js
+++ htdocs/js/samples/data_load.js
@@ -8,7 +8,7 @@ setTimeout(function () {
     chart.load({
         url: '/data/c3_test2.csv'
     });
-}, 500);
+}, 1000);
 
 setTimeout(function () {
     chart.load({
@@ -18,7 +18,7 @@ setTimeout(function () {
             ['data3', 300, 200, 160, 400, 250, 250]
         ]
     });
-}, 1000);
+}, 2000);
 
 setTimeout(function () {
     chart.load({
@@ -32,7 +32,7 @@ setTimeout(function () {
             [90, 220, 320],
         ]
     });
-}, 1500);
+}, 3000);
 
 setTimeout(function () {
     chart.load({
@@ -40,4 +40,4 @@ setTimeout(function () {
             ['data1', null, 30, 20, 50, 40, 60, 50, 100, 200]
         ]
     });
-}, 2000);
+}, 4000);
