e59214846379d30518801aaaff43b61c6a18d8cd
Masayuki Tanaka <masayuki0812@mac.com>
2014-03-07 00:08:51 +0900

    Update sample for x axis tick culling

--- htdocs/js/samples/axes_x_tick_culling.js
+++ htdocs/js/samples/axes_x_tick_culling.js
@@ -1,13 +1,14 @@
 var chart = c3.generate({
     data: {
         columns: [
-            ['sample', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
+            ['sample', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 200, 100, 400, 150, 250]
         ]
     },
     axis: {
         x: {
+            type: 'categorized',
             tick: {
-                culling: false
+                culling: true
                 // for normal axis, default true
                 // for categorized axis, default false
             }
