ced9904664e142fc80035b402ce4bd16a63d32df
Masayuki Tanaka <masayuki0812@mac.com>
2014-03-12 00:44:39 +0900

    Fix files

--- config.rb
+++ config.rb
@@ -68,7 +68,7 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
@@ -82,8 +82,8 @@ activate :sync do |sync|
   sync.fog_provider = 'AWS' # Your storage provider
   sync.fog_directory = 'c3js.org' # Your bucket name
   sync.fog_region = 'ap-northeast-1' # The region your storage bucket is in (eg us-east-1, us-west-1, eu-west-1, ap-southeast-1 )
-  sync.aws_access_key_id = '' # Your Amazon S3 access key
-  sync.aws_secret_access_key = '' # Your Amazon S3 access secret
+  sync.aws_access_key_id = 'AKIAIOQMRC4DM4IDRRJQ' # Your Amazon S3 access key
+  sync.aws_secret_access_key = 'cI4n4jnRC0oxX7u0WWLTgyZKyqdnbD1ZutU5Rc2X' # Your Amazon S3 access secret
   sync.existing_remote_files = 'keep' # What to do with your existing remote files? ( keep or delete )
   #sync.gzip_compression = true # Automatically replace files with their equivalent gzip compressed version
   sync.after_build = false # Disable sync to run after Middleman build ( defaults to true )
