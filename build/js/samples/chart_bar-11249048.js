bcc013a464df2588ade85b14e212ac6da917e024
Masayuki Tanaka <masayuki0812@mac.com>
2014-03-30 10:38:09 +0900

    Update c3.js and sample for bar

--- build/js/samples/chart_bar-11249048.js
+++ build/js/samples/chart_bar-11249048.js
@@ -5,5 +5,12 @@ var chart = c3.generate({
             ['data2', 130, 100, 140, 200, 150, 50]
         ],
         type: 'bar'
+    },
+    bar: {
+        width: {
+            ratio: 0.5 // this makes bar width 50% of length between ticks
+        }
+        // or
+        //width: 100 // this makes bar width 100px
     }
 });
