4e5e99bc3a1a6a9f9e4e5e8b05275f90c213a599
Masayuki Tanaka <masayuki0812@mac.com>
2014-05-05 22:08:38 +0900

    Update sample for api.axis.range

--- build/js/samples/api_axis_range-f5c8f421.js
+++ build/js/samples/api_axis_range-f5c8f421.js
@@ -39,3 +39,15 @@ setTimeout(function () {
 setTimeout(function () {
     chart.axis.range({max: {y: 600, y2: 100}, min: {y: -100, y2: 0}});
 }, 6000);
+
+setTimeout(function () {
+    chart.axis.max({x: 10});
+}, 7000);
+
+setTimeout(function () {
+    chart.axis.min({x: -10});
+}, 8000);
+
+setTimeout(function () {
+    chart.axis.range({max: {x: 5}, min: {x: 0}});
+}, 9000);
