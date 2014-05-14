1147db8e313f2727c61c58fd1d3a165b8bacec78
Masayuki Tanaka <masayuki0812@mac.com>
2014-05-14 18:16:40 +0900

    Add sample for show/hide in getting started

--- source/js/gettingstarted.js
+++ source/js/gettingstarted.js
@@ -150,9 +150,26 @@ var chart4_2 = c3.generate({
     }
 });
 
-
 function example4_2() {
     chart4_2.unload(['data2', 'data3']);
+}
+
+var chart4_3 = c3.generate({
+    bindto: '#chart4_3',
+    data: {
+        columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 50, 20, 10, 40, 15, 25],
+            ['data3', 600, 700, 350, 450, 800, 550]
+        ]
+    }
+});
+
+function example4_3_1() {
+    chart4_3.show(['data2', 'data3']);
+}
+function example4_3_2() {
+    chart4_3.hide(['data2', 'data3']);
 }
 
 c3.generate({
