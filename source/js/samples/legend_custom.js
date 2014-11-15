65e54810f329e50e5e49f7bbd86e741c255dd247
Masayuki Tanaka <masayuki0812@mac.com>
2014-11-15 14:06:55 +0900

    Fix custom legend

--- source/js/samples/legend_custom.js
+++ source/js/samples/legend_custom.js
@@ -16,25 +16,20 @@ function toggle(id) {
     chart.toggle(id);
 }
 
-var legend = d3.select('.container').insert('div', '.chart').attr('class', 'legend');
-legend.append('span').attr('data-id', 'data1').html('data1');
-legend.append('span').attr('data-id', 'data2').html('data1');
-legend.append('span').attr('data-id', 'data3').html('data1');
-
-d3.selectAll('.legend span')
-.each(function () {
-    var id = d3.select(this).attr('data-id');
-    d3.select(this).style('background-color', chart.color(id));
-})
-.on('mouseover', function () {
-    var id = d3.select(this).attr('data-id');
-    chart.focus(id);
-})
-.on('mouseout', function () {
-    var id = d3.select(this).attr('data-id');
-    chart.revert();
-})
-.on('click', function () {
-    var id = d3.select(this).attr('data-id');
-    chart.toggle(id);
-});
+d3.select('.container').insert('div', '.chart').attr('class', 'legend').selectAll('span')
+    .data(['data1', 'data2', 'data3'])
+  .enter().append('span')
+    .attr('data-id', function (id) { return id; })
+    .html(function (id) { return id; })
+    .each(function (id) {
+        d3.select(this).style('background-color', chart.color(id));
+    })
+    .on('mouseover', function (id) {
+        chart.focus(id);
+    })
+    .on('mouseout', function (id) {
+        chart.revert();
+    })
+    .on('click', function (id) {
+        chart.toggle(id);
+    });
