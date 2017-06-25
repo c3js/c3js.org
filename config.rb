1421aceb683b6b0f51beec13284b8a9c5f23fb77
Yoshiya Hinosawa <stibium121@gmail.com>
2017-06-25 20:16:37 +0900

    chore: clean up

--- config.rb
+++ config.rb
@@ -1,44 +1,3 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
 helpers do
   def js_as_plain(id)
     f = open("source/js/samples/" + id + ".js")
@@ -61,46 +20,13 @@ helpers do
   def get_css_name(path)
     path.gsub('.html', '')
   end
-#   def some_helper
-#     "Helping"
-#   end
 end
 
 set :haml, { :ugly => true, :format => :html5 }
-
 set :css_dir, 'css'
-
 set :js_dir, 'js'
-
 set :images_dir, 'img'
 
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
   activate :asset_hash, :ignore => %r{^js/ace/.*}
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
-
-# Activate sync extension
-activate :sync do |sync|
-  sync.fog_provider = 'AWS' # Your storage provider
-  sync.fog_directory = 'c3js.org' # Your bucket name
-  sync.fog_region = 'ap-northeast-1' # The region your storage bucket is in (eg us-east-1, us-west-1, eu-west-1, ap-southeast-1 )
-  sync.aws_access_key_id = ENV['AWS_KEY'] # Your Amazon S3 access key
-  sync.aws_secret_access_key = ENV['AWS_SECRET'] # Your Amazon S3 access secret
-  sync.existing_remote_files = 'keep' # What to do with your existing remote files? ( keep or delete )
-  #sync.gzip_compression = true # Automatically replace files with their equivalent gzip compressed version
-  sync.after_build = false # Disable sync to run after Middleman build ( defaults to true )
-end
-Fog.credentials = {:path_style => true}
